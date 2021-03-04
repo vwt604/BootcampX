67b4e2a70c187a7eb76a91246e3f7421baa91c79
Vanessa Y <vyeung.wt@gmail.com>
2021-03-04 06:25:13 +0000

    sanitize queries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,28 +9,20 @@ const pool = new Pool({
 });
 
 
-//pool.query is a function that accepts an SQL query as a JavaScript string
-//returns a promise that contains our result when the query is successful
+// pool.query is a function that accepts an SQL query as a JavaScript string
+// returns a promise that contains our result when the query is successful
+const cohort = process.argv[2];
+const limit = process.argv[3];
 pool.query(`
 SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+`, [`%${cohort}%`, limit])
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
   })
 }).catch(err => console.error('query error', err.stack));
 
-/*
-returns as a Javascript Object:
-[ { id: 1, name: 'Armand Hilll', cohort_id: 1 },
-  { id: 2, name: 'Stephanie Wolff', cohort_id: 1 },
-  { id: 3, name: 'Stan Miller', cohort_id: 1 },
-  { id: 4, name: 'Elliot Dickinson', cohort_id: 1 },
-  { id: 5, name: 'Lloyd Boehm', cohort_id: 1 } ]
-  */
-
-   
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -11,13 +11,14 @@ const pool = new Pool({
 
 //pool.query is a function that accepts an SQL query as a JavaScript string
 //returns a promise that contains our result when the query is successful
+const cohort = process.argv[2];
 pool.query(`
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM teachers 
 JOIN assistance_requests on teachers.id = teacher_id
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
+WHERE cohorts.name LIKE '%${cohort}%'
 ORDER BY teachers.name
 `)
 .then(res => {
@@ -27,3 +28,5 @@ ORDER BY teachers.name
 }).catch(err => console.error('query error', err.stack));
 
 //run: node teachers.js JUL02
+
+
